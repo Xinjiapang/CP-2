5ff22d1c55b14faf86afa7912786aabfeb49a2ea
Xinjiapang <47263250+Xinjiapang@users.noreply.github.com>
2020-01-27 09:30:30 -0800

    Create puzzle1.js

---
+++ puzzle1.js
@@ -0,0 +1,24 @@
+Project 1: Arduino color sorter ---https://www.youtube.com/watch?v=QUQta4f_87E  [00:13- 00:30]
+  Input(Data): 
+    - RGB of the skittle (Color Sensor)
+    - Servo motor 1 rotation angle (move the skittle under the color sensor)
+    - Delay time of servo motor 1 
+    - Servo motor 2 rotation to a particular position (transfer the skittle to a particular container)
+  Render:
+    - The angle servo motor (track) rotates
+  Simulation:
+    - N/A
+  Event:
+    - On/ Off switch
+    - Skittle under the color sensor (trigger motor 2)
+
+Project 2: ‘Stack’ Game Player Robot ---https://www.youtube.com/watch?v=0XTcJ5-0u00  [00:44- 01:09]
+  Input(Data): 
+    - Frequency of rotating servo motor
+    - Position of rotating servo motor
+  Render:
+    - The rotation of the capacitive pen
+  Simulation:
+    - N/A
+  Event:
+    - On/off switch
